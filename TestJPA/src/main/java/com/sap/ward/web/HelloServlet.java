d00dc56f33e9e90199bd62602f3d8d78a9afcd19
wardpeng <ward.peng@gmail.com>
2017-09-18 14:37:33 +0800

    Backup for pom/xml http://blog.csdn.net/starshus/article/details/22709923 vesion

--- TestJPA/src/main/java/com/sap/ward/web/HelloServlet.java
+++ TestJPA/src/main/java/com/sap/ward/web/HelloServlet.java
@@ -1,31 +1,25 @@
 package com.sap.ward.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class HelloServlet extends HttpServlet
+public class HelloServlet
+// extends HttpServlet
 {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-	{
-		PrintWriter out = resp.getWriter();
-		out.println("<html>");
-		out.println("<head><title>MLDNJAVA</title></head>");
-		out.println("<body>");
-		out.println("<h1>HELLO WORLD</h1>");
-		out.println("</body>");
-		out.println("</html>");
-		out.close();
-	}
+	// /**
+	// *
+	// */
+	// private static final long serialVersionUID = 1L;
+	//
+	// public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	// throws ServletException, IOException
+	// {
+	// PrintWriter out = resp.getWriter();
+	// out.println("<html>");
+	// out.println("<head><title>MLDNJAVA</title></head>");
+	// out.println("<body>");
+	// out.println("<h1>HELLO WORLD</h1>");
+	// out.println("</body>");
+	// out.println("</html>");
+	// out.close();
+	// }
 
 }
